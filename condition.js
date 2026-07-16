b2a4dc0df7b62fb1827f27e380e12dbd92712ffd
amioswe <amioswe@gmail.com>
2026-07-17 01:19:57 +0600

    conditional learn

---
+++ condition.js
@@ -0,0 +1,31 @@
+// const a =10;
+// const b=20;
+// console.log( a != b);
+
+
+
+
+    // const price = 392;
+    // const money = 5473;
+    // if (price != money) {
+
+    //     console.log('I wiil all shoping now ');
+
+    // }
+
+
+    const age=10;
+ if( age>=12){
+
+console.log('You are go');
+
+
+}
+else if(age<=12){
+
+    console.log('You are not allowed ');
+
+}
+else{
+    console.log('Nothing');
+}
