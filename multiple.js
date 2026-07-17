506b1b1746fae92c46a07ba30225732c0ad2fbe0
amioswe <amioswe@gmail.com>
2026-07-17 14:52:04 +0600

    grade marking conditions

---
+++ multiple.js
@@ -0,0 +1,42 @@
+// const salary=75000;
+// const isBcs=false;
+// const hasCar=true;
+
+// if(salary>=80000 || isBcs ){
+
+// console.log('Biyer jonno prostut');
+
+
+// }
+// else{
+//     console.log('Tor kopale biye nai !!');
+// }
+
+const marks=50;
+
+if(marks >=80){
+    console.log('Grade:A+');
+}
+else if( marks >=70){
+
+    console.log('Grade:A');
+
+}
+else if( marks >=60){
+
+    console.log('Grade:B');
+}
+else if( marks >=50){
+
+    console.log('Grade:D');
+}
+
+else if( marks >=40){
+
+    console.log('Grade:pass');
+}
+else{
+    console.log('Tui Fail student');
+}
+
+
