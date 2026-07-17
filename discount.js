80d60773b878d44c568b83cf143ee2a2b06ecf5a
amioswe <amioswe@gmail.com>
2026-07-17 15:17:30 +0600

    discount formula test

---
+++ discount.js
@@ -0,0 +1,18 @@
+const amount=5000;
+
+if(amount>=3000){
+const discount=30;
+const afterdiscount=amount/100*discount;
+console.log(afterdiscount);
+
+}
+else if(amount>2500){
+
+    const discount=20;
+    const afterdiscount=amount/100*discount;
+    console.log(afterdiscount);
+
+}
+else{
+    console.log('Discount are not avlaible');
+}
